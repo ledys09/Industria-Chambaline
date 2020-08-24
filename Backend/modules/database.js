09ec02d8269b26066337df8fa1abe278cbffb7ec
ledys09 <alvaradoestefany51@yahoo.es>
2020-08-23 20:44:32 -0600

    [UPDATE] db

--- Backend/modules/database.js
+++ Backend/modules/database.js
@@ -1,13 +1,12 @@
 // Database: Conexión con la base de datos de Chambaline.
-// mongoose = require('mongoose');
-const mongoClient = require("mongodb").MongoClient;
+mongoose = require('mongoose');
 require("dotenv").config({ path: "./config.env" });
 
 const cl= process.env.USER;
 
 class Database{
     constructor(){
-        mongoClient.connect(`mongodb+srv://${cl}:${cl}@chambalinedb.zpwuj.mongodb.net/${cl}?retryWrites=true&w=majority`, { useNewUrlParser: true })
+        mongoose.connect(`mongodb+srv://${cl}:${cl}@chambalinedb.zpwuj.mongodb.net/${cl}?retryWrites=true&w=majority`, { useNewUrlParser: true })
 /*         mongoClient.connect(`mongodb+srv://chambaline:chambaline@chambalinedb.zpwuj.mongodb.net/chambaline?retryWrites=true&w=majority`, { useNewUrlParser: true })
  */        .then(()=>{
             console.log('Conectado a la base de datos de Chambaline');
